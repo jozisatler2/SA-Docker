9565b6205111881e4f00dba9a61cad948dcd8bae
jozisatler2 <jozi.satler04@gmail.com>
2025-03-18 18:35:57 +0100

    Added image capture every 10s

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import cv2
+import threading
+import time
+import os
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
 @app.route('/')
-def home():
+def home():    
     return "Hello"
 
+def capture():
+    while True:
+        _, image = camera.read()
+        os.system("rm -f image.png")
+        cv2.imwrite('image.png', image)
+        time.sleep(10)
+
 if __name__ == '__main__':
+    thread = threading.Thread(target=capture)
+    thread.daemon = True
+    thread.start()
     app.run()
