a356d2ec80736ee631819c52f849e61674a1f3c4
jozisatler2 <jozi.satler04@gmail.com>
2025-03-18 19:27:43 +0100

    Added socket emit to clients

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 from flask import Flask, send_from_directory
+from flask_socketio import SocketIO, emit
 import cv2
 import threading
 import time
 import os
 
-app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
-@app.route('/')
-def home():    
-    return '<img src="/image.png">'
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# @app.route('/')
+# def home():    
+#     return '<img src="/image.png">'
 
 @app.route('/image.png')
 def serve_image():
@@ -19,10 +22,11 @@ def capture():
     while True:
         _, image = camera.read()
         cv2.imwrite('image.png', image)
-        time.sleep(1)
+        socketio.emit('update')
+        time.sleep(10)
 
 if __name__ == '__main__':
     thread = threading.Thread(target=capture)
     thread.daemon = True
     thread.start()
-    app.run()
+    socketio.run(app)
